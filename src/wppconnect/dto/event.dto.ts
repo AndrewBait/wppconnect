a82358cf629b29ded1fe66cc0a431185ad016e3c
AndrewBait <andrewsilva18@hotmail.com>
2024-08-29 18:10:24 -0300

    swagger

--- src/wppconnect/dto/event.dto.ts
+++ src/wppconnect/dto/event.dto.ts
@@ -4,10 +4,10 @@ export class EventDto {
   @ApiProperty({ example: 'example_id', description: 'ID da mensagem recebida.' })
   id: string;
 
-  @ApiProperty({ example: 'Esta é uma mensagem de teste', description: 'Conteúdo da mensagem recebida.' })
+  @ApiProperty({ example: 'Mensagem de teste', description: 'Conteúdo da mensagem recebida.' })
   body: string;
 
-  @ApiProperty({ example: '559999999999999', description: 'Número de telefone do remetente no formato internacional.' })
+  @ApiProperty({ example: '5512997886488', description: 'Número de telefone do remetente no formato internacional.' })
   from: string;
 
 }
--- src/wppconnect/dto/send-message.dto.ts
+++ src/wppconnect/dto/send-message.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SendMessageDto {
-  @ApiProperty({ example: '5599999999999', description: 'Número de telefone do destinatário no formato internacional.' })
+  @ApiProperty({ example: '5512988378188', description: 'Número de telefone do destinatário no formato internacional.' })
   to: string;
 
   @ApiProperty({ example: 'Olá!', description: 'Conteúdo da mensagem a ser enviada.' })
